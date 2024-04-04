db36b6989fe52637e820c32432ae3eedd0e0251c
mik3yyy <mikeokpechi@gmail.com>
2024-04-04 16:23:58 +0100

    chnage

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/.well-known', express.static('public'));
 
-app.listen(3000, () => {
+app.listen(3000|| process.env.PORT, () => {
   console.log('Server started on port 3000');
 });
